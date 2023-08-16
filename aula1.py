59a2aac580bedce14656f9f9e07ac91a289bcbef
RonaldoMagalhaes <ronaldo.magalhaes1@gmail.com>
2023-08-16 10:40:18 -0300

    aula1

--- aula1.py
+++ aula1.py
@@ -1,1 +1,13 @@
+"""
+    Doc string - nao eh um comentario, o interpretador
+    do python ler isso
+
+    utilizados para explicar o seu codigo
+"""
+
+# permite escrever um comentario
+print(123)
+print(456)
+
+
 
