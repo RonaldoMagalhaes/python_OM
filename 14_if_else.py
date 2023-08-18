f8a59532025534bcb86b6411d808b0b1c331bbf6
RonaldoMagalhaes <ronaldo.magalhaes1@gmail.com>
2023-08-17 22:54:41 -0300

    aulas 13 e 14

---
+++ 14_if_else.py
@@ -0,0 +1,15 @@
+print("#"*80)
+print()
+
+entrada = input("Voce quer 'entrar' ou 'sair' ?: ").lower()
+
+if entrada == 'entrar':
+    print("Voce digitou 'entrar'...")
+elif entrada == 'sair':
+    print("Voce digitou 'sair'...")
+else:
+    print("Opcao invalida")
+
+
+print()
+print("#"*80)
