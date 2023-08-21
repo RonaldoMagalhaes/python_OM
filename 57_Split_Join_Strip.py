9677a164d14c28f7e0842e64a7d93aca3abbebd5
RonaldoMagalhaes <ronaldo.magalhaes1@gmail.com>
2023-08-20 21:09:50 -0300

    split, lista_dentro_de_lista, interpretador, op_ternario

--- 57_Split_Join_Strip.py
+++ 57_Split_Join_Strip.py
@@ -3,14 +3,17 @@ split e join com list e str
 split - divide uma string (list)
 join - une uma string
 """
+
+
+
 frase = '   Olha só que   , coisa interessante          '
-lista_frases_cruas = frase.split(',')
 
-lista_frases = []
-for i, frase in enumerate(lista_frases_cruas):
-    lista_frases.append(lista_frases_cruas[i].strip())
+lista_palavras = frase.split()
+print(lista_palavras)
 
-# print(lista_frases_cruas)
-# print(lista_frases)
-frases_unidas = ', '.join(lista_frases)
+for i, frase in enumerate(lista_palavras):
+    print(lista_palavras[i].strip())
+
+
+frases_unidas = "&".join("ABC")
 print(frases_unidas)
